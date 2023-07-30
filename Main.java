ad2f3bdd6e86f4cda25635a3227aa0bbfafa5bac
wlf94904 <127936526+wlf94904@users.noreply.github.com>
2023-07-30 16:33:57 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,93 @@
-package hello3;
+package hello4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.Socket;
 
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("hello world");
-		//构造一个字节数组，它有10个字节
-		byte[] buf=new byte[10];
-		//遍历数组
-		for(int i=0;i<buf.length;i++) {
-			buf[i]=(byte)i;//int需要强制转换成字节
-			
-			
-		}
-		try {
-			//不仅仅可以写字节用DataOutputStream
-			PrintWriter out=new PrintWriter(
-					new BufferedWriter(
-							new OutputStreamWriter(
-									new FileOutputStream("a.txt"))));
-//			FileOutputStream out=new FileOutputStream("a.dat");//建立一个FileOutputStream的对象
-			//用FileOutputStream只能写字节
-//			out.write(buf);
-			int i=12;
-			out.println(i);
-			out.close();
-			BufferedReader in=new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("src/hello3/Main.java")));
-			String line;
-			while((line=in.readLine())!=null) {
-				System.out.println(line);
-			}
-//			DataInputStream in=new DataInputStream(
-//					new BufferedInputStream(
-//							new FileInputStream("a.dat")));
-//			int j=in.readInt();
-//			System.out.println(j);
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		//文件名叫a.dat
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-
+class Student implements Serializable{
+	private String name;
+	private int age;
+	private int grade;
+	
+	//构造
+	public Student(String name,int age,int grade) {
+		this.name=name;
+		this.age=age;
+		this.grade=grade;
 	}
-
+	
+	public String toString() {
+		return name+" "+age+" "+grade;
+	}
 }
+
+public class Main{
+	public static void main(String[] args) {
+		try {
+			Student s1=new Student("John",18,5);
+			System.out.println(s1);
+			//构造一个可以直接把s1写到文件里的方式
+			ObjectOutputStream out=new ObjectOutputStream(
+					new FileOutputStream("a.dat"));
+			out.writeObject(s1);
+			out.close();
+			//读出来
+			ObjectInputStream in=new ObjectInputStream(
+					new FileInputStream("a.dat"));
+			Student s2=(Student)in.readObject();
+			System.out.println(s2);
+			in.close();
+			System.out.println(s1==s2);//判断是否指向同一个对象
+		}catch(IOException e){
+			e.printStackTrace();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		try {
+//			Socket socket=new Socket(InetAddress.getByName("localhost"),12345);
+//			//写文本要有PrintWriter
+//			PrintWriter out=new PrintWriter(
+//					new BufferedWriter(
+//							new OutputStreamWriter(
+//									socket.getOutputStream())));
+//			out.println("hello");
+//			out.flush();
+//			BufferedReader in=new BufferedReader(
+//					new InputStreamReader(
+//							socket.getInputStream()));
+//			out.close();
+//			
+//			//从远端服务器收东西
+//			BufferedReader in=new BufferedReader(
+//					new InputStreamReader(
+//							socket.getInputStream()));
+//			String line;
+//			line=in.readLine();
+//			System.out.println(line);
+//			socket.close();
+//		}catch(IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//
+//	}
+//
+//}
